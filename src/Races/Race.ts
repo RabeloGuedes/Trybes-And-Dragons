eac10ef3f4610b1c8949815d5e2035e73bd8c802
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-09-22 17:51:11 -0300

    feat: Cria a classe abstrata Race.

---
+++ src/Races/Race.ts
@@ -0,0 +1,25 @@
+export default abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+  private static _counter = 0;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+    Race._counter += 1;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
