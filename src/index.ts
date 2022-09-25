547ed402bf85edcc4a347e69fda5661a2055e4f9
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-09-25 19:43:23 -0300

    feat: Cria instâncias das classes, Character, Monster, Dragon, PVP, PVE e uma função runBattles.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,35 @@
-import express from 'express';
+import Battle, { PVP, PVE } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
 
-const PORT = 3000;
+const player1 = new Character('Gandalf');
+const player2 = new Character('Harray Potter');
+const player3 = new Character('Luke Skywalker');
 
-const app = express();
+const monster1 = new Monster();
+const monster2 = new Dragon();
 
-app.listen(PORT, () => console.log(`Escutando na porta: ${PORT}`));
+const pvp = new PVP(player2, player3);
+
+const pve = new PVE(player1, [monster1, monster2]);
+
+player1.levelUp();
+player1.levelUp();
+
+const runBattles = (battles: Battle[]): void => {
+  battles.forEach((battle) => {
+    battle.fight();
+  });
+};
+
+export {
+  player1,
+  player2,
+  player3,
+  monster1,
+  monster2,
+  pvp,
+  pve,
+  runBattles,
+};
